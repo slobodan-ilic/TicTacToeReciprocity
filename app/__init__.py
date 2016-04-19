63bd2553fe924a626860714d54ae91a887e823c1
slobodan.ilic <slobodan.a.ilic@gmail.com>
2016-04-19 11:08:40 +0200

    Created basic project structure for DB management.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from config import config
+from .model import db
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    db.init_app(app)
 
     return app
