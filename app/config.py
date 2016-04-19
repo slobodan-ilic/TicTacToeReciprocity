aa906d65256c9bdfc331602dfc7bc9f28b7dc674
slobodan.ilic <slobodan.a.ilic@gmail.com>
2016-04-19 12:50:21 +0200

    Added simplest DB structure, tested for db creation.

--- app/config.py
+++ app/config.py
@@ -1,18 +1,20 @@
 from uuid import uuid4
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 secret_key = str(uuid4())
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or secret_key
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DEV_DATABASE_URL') or
-                               'sqlite:///data/test.db')
+                               'sqlite:///../ttt_recip.db')
 
 config = {
     'development': DevelopmentConfig
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import create_app, db
 from flask.ext.script import Manager, Shell
 
 app = create_app('development')
@@ -6,7 +6,7 @@ manager = Manager(app)
 
 
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db)
 manager.add_command('shell', Shell(make_context=make_shell_context))
 
 
