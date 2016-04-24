43213a4c5d4a2f278cce6d93f9b8a609b3c5ce8a
slobodan.ilic <slobodan.a.ilic@gmail.com>
2016-04-24 12:26:49 +0200

    Extract *.js code to *.js file.

--- app/static/js/network_player.js
+++ app/static/js/network_player.js
@@ -21,3 +21,32 @@ function SetupSocketIOConnection() {
         alert(err.message);
     }
 }
+
+function UpdateBoardOnSocketIO(data) {
+    var result = data.result;
+    if ((result != 'Player X Turn') &&
+            (result != 'Player O Turn')) {
+        $('#frm_get_result').submit();
+    }
+    var m = data.updated_board.length;
+    for (var i=0; i<m; i++) {
+        var row = data.updated_board[i];
+        var n = row.length;
+        for (var j=0; j<n; j++) {
+            var index = i * m + j;
+            var pos = row[j];
+            var html;
+            var strBtnSelector = '#btn_' + index;
+            if (pos[2] == 'O') {
+                html = '<i class="glyphicon glyphicon-unchecked"></i>';
+                $(strBtnSelector).html(html);
+                $(strBtnSelector).attr('disabled', 'disabled');
+            }
+            else if (pos[2] == 'X') {
+                html = '<i class="glyphicon glyphicon-remove"></i>';
+                $(strBtnSelector).html(html);
+                $(strBtnSelector).attr('disabled', 'disabled');
+            }
+        }
+    }
+}
